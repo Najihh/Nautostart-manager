d75a0e2e840c0fda772a780db269c1a155148e4d
Najih <muhammad.iqbal.najih@gmail.com>
2020-03-18 22:36:10 +0700

    implementation lib

--- app/src/main/java/com/senjacreative/nautostart/MainActivity.kt
+++ app/src/main/java/com/senjacreative/nautostart/MainActivity.kt
@@ -2,11 +2,17 @@ package com.senjacreative.nautostart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.senjacreative.autostart.NAutoStartPermissionHelper
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val success = NAutoStartPermissionHelper.getInstance().getAutoStartPermission(this@MainActivity)
+        var message = "Failed"
+        if (success) message = "Successfull"
+        Toast.makeText(this@MainActivity, "Action $message", Toast.LENGTH_SHORT).show()
     }
 }
